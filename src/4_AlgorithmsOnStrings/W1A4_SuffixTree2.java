cd33a97871d32d52a30e7904fb4cffe308634536
Jérôme MANCHON <jerome.manchon@live.fr>
2016-11-20 22:47:33 +0100

    code cleaning

--- src/4_AlgorithmsOnStrings/W1A4_SuffixTree2.java
+++ src/4_AlgorithmsOnStrings/W1A4_SuffixTree2.java
@@ -2,10 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.StringTokenizer;
 
 public class W1A4_SuffixTree2 {
